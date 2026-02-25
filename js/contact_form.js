0699724286d6d5e48d31021f9b0fe60d3768363d
Morion178 <musatmario00@gmail.com>
2026-02-25 12:50:08 +0200

    Lab1: HTML and CSS review

---
+++ js/contact_form.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    const nume = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mesaj = document.getElementById("message").value;
+    console.log(nume, email, mesaj);
+}
